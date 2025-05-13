f5f960cf59e23e94d6ea66d4839b728890a5f902
trongnguyen00 <trongnguyen00@github.com>
2025-05-13 14:34:27 +0700

    clean

--- send_robot.py
+++ send_robot.py
@@ -5,10 +5,9 @@ from email.mime.text import MIMEText
 import smtplib
 from premailer import transform
 
-# Đọc dữ liệu test từ file CSV/json (ví dụ sau dùng CSV)
 with open("latest_result.txt", "r") as f:
     csv_path = f.read().strip()
-df = pd.read_csv(csv_path)  # hoặc pd.read_json(...)
+df = pd.read_csv(csv_path)
 
 def format_status(status):
     s = str(status).strip().lower()
@@ -24,10 +23,8 @@ def format_status(status):
 df["Status"] = df["Status"].apply(format_status)
 df = df.fillna("N/A")
 
-# Convert DataFrame thành bảng HTML
 table_html = df.to_html(index=False, escape=False, border=1)
 
-# Chèn style trực tiếp vào bảng
 table_html = table_html.replace(
     '<table border="1" class="dataframe">',
     '''<table border="1" cellspacing="0" cellpadding="6" style="
@@ -42,35 +39,29 @@ table_html = table_html.replace(
     '<tr style="text-align: right;">', '<tr>'
 )
 
-# Đọc template và thay thế
 with open("template.html", "r") as f:
     html_template = Template(f.read())
 
 email_body = html_template.safe_substitute(table=table_html)
 
-# Soạn email
 msg = MIMEMultipart("alternative")
 msg["Subject"] = "Robot Test Report"
 msg["From"] = "nguyenkimtrong43925@gmail.com"
 
-# Gửi đến nhiều người
 recipients_to = ["nguyen.trong@kaonbroadband.com", "jungyu.choi@kaonbroadband.com"]
 # recipients_cc = ["bob@example.com"]
 
 msg["To"] = ", ".join(recipients_to)
 # msg["Cc"] = ", ".join(recipients_cc)
 
-# Gắn nội dung HTML
 msg.attach(MIMEText(email_body, "html"))
 
-# Gửi email qua Gmail
 smtp_server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
 smtp_server.login("nguyenkimtrong43925@gmail.com", "tvgmiggtjjrrzbsg")
 
-# Gửi đến tất cả người nhận
 # all_recipients = recipients_to + recipients_cc
 all_recipients = recipients_to
 smtp_server.sendmail(msg["From"], all_recipients, msg.as_string())
 
 smtp_server.quit()
-print("✅ Email with styled HTML table sent to multiple recipients.")
+print("Email with styled HTML table sent to multiple recipients.")
