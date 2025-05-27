948a2689c2edcf7b4d8beaa87bfc4d53a3e7eed3
trongnguyen00 <trongnguyen00@github.com>
2025-05-27 18:33:06 +0700

    Merge all keyword to oneclass CustomTelnet

--- library/CustomKeywords.py
+++ library/CustomKeywords.py
@@ -1,7 +1,7 @@
 from robotlibcore import DynamicCore
 from robot.api.deco import library
 
-from library.CustomTelnet import *
+from library.CustomTelnet import CustomTelnet
 from library.terminal.TopologyLoader import TopologyLoader
 from library.utils.TableVerificationLibrary import TableVerificationLibrary
 from library.cdr.CDRouterLibrary import CDRouterLibrary
@@ -11,16 +11,27 @@ from library.terminal.dal.keywords.system import SystemKeywords
 
 @library(scope='GLOBAL')
 class CustomKeywords(DynamicCore):
+    """Custom Keywords library that combines various test automation libraries.
+    
+    This library provides access to:
+    - Telnet connection management and command execution
+    - Topology loading and device information
+    - Table verification utilities
+    - CDRouter testing capabilities
+    - Ixia testing capabilities
+    - GPON and System specific keywords
+    """
+    
     def __init__(self):
+        """Initialize the library with all required components."""
+        # Initialize core components
         self.topology_loader = TopologyLoader()
         self.custom_telnet = CustomTelnet()
-        self.telnet_group_handler = TelnetCommandGroupHandler(self.custom_telnet)
 
+        # Initialize all libraries
         libraries = [
             self.topology_loader,
             self.custom_telnet,
-            self.telnet_group_handler,
-            CustomTelnetConnection(),
             TableVerificationLibrary(),
             CDRouterLibrary(),
             IxiaLibrary(),
